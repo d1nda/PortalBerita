5e7f8aafeb8c61a3b1919aaac57afaa03a1da20e
d1nda <110410691+d1nda@users.noreply.github.com>
2022-08-02 15:53:13 +0700

    Add files via upload

---
+++ portalberita/footer.php
@@ -0,0 +1,16 @@
+	</div>
+
+</div>
+
+<footer>
+	<div class="wrap">
+		<div class="pd10">
+			<div class="footer pd10">
+				Copyright @ 2022 | Tugas Pemrograman Web
+			</div>
+		</div>
+	</div>
+</footer>
+
+</body>
+</html>
